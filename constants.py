b4a9fd37c1509110e26aa55d9ab675dc3c4d827e
Aazib Abdullah <aaziba@cpp.edu>
2026-02-13 12:14:25 -0800

    Moved all constants to constants.py

---
+++ constants.py
@@ -0,0 +1,15 @@
+# ======================================
+# Constants
+# ======================================
+WIDTH = HEIGHT = 600
+SQUARE_SIZE = WIDTH // 3
+LINE_THICKNESS = 10
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+
+EMPTY = -1
+PLAYER_1 = 0  # X (Blue)
+PLAYER_2 = 1  # O (Red)
--- main.py
+++ main.py
@@ -1,23 +1,7 @@
 import pygame
 import sys
-
+from constants import *
 pygame.init()
-
-# ======================================
-# Constants
-# ======================================
-WIDTH = HEIGHT = 600
-SQUARE_SIZE = WIDTH // 3
-LINE_THICKNESS = 10
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-BLUE = (0, 0, 255)
-RED = (255, 0, 0)
-
-EMPTY = -1
-PLAYER_1 = 0  # X (Blue)
-PLAYER_2 = 1  # O (Red)
 
 # ======================================
 # Setup
